deda3f67be785cc630e8d635bcc6e73efff279c9
aholiveira <98539014+aholiveira@users.noreply.github.com>
2022-09-05 12:12:53 +0100

    Fix session handling

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         <hr />
         <?php
         printf("<span class='RCS'>\$ Id: %s$</span>", GITHASH);
+        printf("<span class='RCS'>Session expires at %s</span>", date("Y-m-d H:i:s", $_SESSION['expires']));
         ?>
     </div>
 </footer>
