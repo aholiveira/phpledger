2e071057ac6da10493c9f16d5291d65a61e19383
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-07-30 22:24:32 +0100

    Change session expired logic and add timezone detection

--- contas_config.php
+++ contas_config.php
@@ -12,7 +12,6 @@ if (!defined("ROOT_DIR")) {
     include __DIR__ . "/prepend.php";
 }
 config::init(__DIR__ . '/config.json');
-
 if (isset($_SESSION['expires']) && $_SESSION['expires'] < time()) {
     $_SESSION = [];
     if (ini_get("session.use_cookies")) {
@@ -21,21 +20,36 @@ if (isset($_SESSION['expires']) && $_SESSION['expires'] < time()) {
     }
 }
 if (!isset($_SESSION['user'])) {
-    ?>
-    <!DOCTYPE html>
-    <html lang="pt-PT">
-    <?php include "header.php"; ?>
-    <meta http-equiv="REFRESH" content="5; URL=index.php">
+    if (!headers_sent()) {
+        header("Location: index.php");
+        exit;
+    } else {
+        ?>
+        <!DOCTYPE html>
+        <html lang="pt-PT">
+        <?php include "header.php"; ?>
+        <meta http-equiv="refresh" content="5;url=index.php">
+        <script>
+            window.addEventListener("DOMContentLoaded", () => {
+                myalert("Your session has expired! Please login again.", () => {
+                    window.location.href = "index.php";
+                });
+            });
+        </script>
+        </head>
 
-    <body>
-        <p>Your session has expired! Please login again.</p>
-    </body>
+        <body> </body>
 
-    </html>
-    <?php
+        </html>
+    <?php }
     exit(1);
 } else {
     $_SESSION['expires'] = time() + 3600;
+    if (!isset($_SESSION['timezone']) && isset($_COOKIE['timezone']) && in_array($_COOKIE['timezone'], timezone_identifiers_list())) {
+        $_SESSION['timezone'] = $_COOKIE['timezone'];
+    }
+    $tz = $_SESSION['timezone'] ?? config::get("timezone");
+    date_default_timezone_set(in_array($tz, timezone_identifiers_list(), true) ? $tz : 'UTC');
     session_write_close();
     $object_factory = new object_factory();
     $view_factory = new view_factory();
