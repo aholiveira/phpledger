5024de939a83cef6487a9dd9787ef4de3d007a84
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-07-30 22:24:13 +0100

    Change session expired logic and add timezone detection

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
 <title>
     <?= htmlspecialchars((!empty($pagetitle) ? "$pagetitle - " : "") . config::get("title")) ?>
 </title>
+<script>
+    document.cookie = "timezone=" + Intl.DateTimeFormat().resolvedOptions().timeZone + "; path=/";
+</script>
+
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="styles.css">
