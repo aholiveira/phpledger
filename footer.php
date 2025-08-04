fb4d0c6251578d0fe9030e76dcf004a5ab65fb4e
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-08-04 23:19:07 +0100

    Fix ledger entries table overflow

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <footer>
     <div class='footer'>
-        <hr>
         <span class='RCS'>$ Id: <?php print constant("GITHASH"); ?> $</span>
         <span class='RCS'>Session expires at <?php print date("Y-m-d H:i:s", $_SESSION['expires']); ?></span>
     </div>
