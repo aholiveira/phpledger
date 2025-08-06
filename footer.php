4328153170eff5a52c356a7374e48b84188fc104
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-08-06 15:03:12 +0100

    Change version footer with a link to the project

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class='footer'>
-        <span class='RCS'>$ Version: <?= VERSION ?> $</span>
+        <span class='RCS'>Version <a href="https://github.com/aholiveira/phpledger"><?= VERSION ?></a></span>
         <span class='RCS'>Session expires at <?= date("Y-m-d H:i:s", $_SESSION['expires']) ?></span>
     </div>
 </footer>
