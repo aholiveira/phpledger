dc64fa03ad01c939c02b967b0b54feed7519a794
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-08-06 13:31:07 +0100

    Change footer to display version instead of git hash

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class='footer'>
-        <span class='RCS'>$ Id: <?php print constant("GITHASH"); ?> $</span>
-        <span class='RCS'>Session expires at <?php print date("Y-m-d H:i:s", $_SESSION['expires']); ?></span>
+        <span class='RCS'>$ Version: <?= VERSION ?> $</span>
+        <span class='RCS'>Session expires at <?= date("Y-m-d H:i:s", $_SESSION['expires']) ?></span>
     </div>
 </footer>
