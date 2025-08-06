e2839e521f20be792d4dd7e89b68f0e53da167ec
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-08-06 17:35:04 +0100

    Move l10n to separate files

--- prepend.php
+++ prepend.php
@@ -55,12 +55,14 @@ SessionManager::start();
 $requested_lang = strtolower($_GET['lang'] ?? '');
 $browser_lang = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? '');
 
-if ($requested_lang === 'en')
+if ($requested_lang === 'en-us')
     $lang = 'en-US';
-elseif ($requested_lang === 'pt')
+elseif ($requested_lang === 'pt-pt')
     $lang = 'pt-PT';
 else
     $lang = str_starts_with($browser_lang, 'en') ? 'en-US' : 'pt-PT';
+
+$l10n = require ROOT_DIR . "/lang/{$lang}.php";
 
 /**
  * Prints variable
--- update.php
+++ update.php
@@ -21,43 +21,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && ($_POST['action'] ?? null) === 'Act
 
 $needs_update = !$data_storage->check();
 $message = nl2br(htmlspecialchars($data_storage->message(), ENT_QUOTES, 'UTF-8'));
-
-$strings = [
-    'pt-PT' => [
-        'title' => "Actualização necessária",
-        'db_needs_update' => "A base de dados necessita de ser actualizada.",
-        'cannot_use_app' => "Não poderá utilizar a aplicação sem concluir esta actualização.",
-        'start_update' => "Prima o botão abaixo para iniciar o processo.",
-        'form_button' => "Actualizar",
-        'db_updated' => "A base de dados foi actualizada com sucesso.",
-        'redirecting' => "Será redireccionado para o ecrã de login em 5 segundos.",
-        'db_ok' => "A base de dados está actualizada.",
-        'go_login' => "Pode aceder à aplicação a partir do <a style='padding: 0' href='index.php'>ecrã de login</a>.",
-        'update_fail' => "Falha na actualização. Verifique as permissões do utilizador.",
-        'error_msg' => "Mensagem de erro:"
-    ],
-    'en-US' => [
-        'title' => "Update Required",
-        'db_needs_update' => "The database requires an update.",
-        'cannot_use_app' => "You cannot use the application until this update is completed.",
-        'start_update' => "Click the button below to begin the update process.",
-        'form_button' => "Update",
-        'db_updated' => "The database was successfully updated.",
-        'redirecting' => "You will be redirected to the login screen in 5 seconds.",
-        'db_ok' => "The database is up to date.",
-        'go_login' => "You can access the application from the <a style='padding: 0' href='index.php'>login screen</a>.",
-        'update_fail' => "Update failed. Please check user permissions.",
-        'error_msg' => "Error message:"
-    ]
-];
-$pagetitle = $strings[$lang]['title'];
+$pagetitle = $l10n['update_needed'];
 ?>
 <!DOCTYPE html>
-<html lang="<?= $lang === 'en' ? 'en-US' : 'pt-PT' ?>">
+<html lang="<?= $lang === 'en-us' ? 'en-US' : 'pt-PT' ?>">
 
 <head>
     <?php include "header.php"; ?>
-    <title><?= $strings[$lang]['title'] ?></title>
+    <title><?= $l10n['update_needed'] ?></title>
     <?php if ($update_result): ?>
         <meta http-equiv="refresh" content="5;url=index.php">
     <?php endif; ?>
@@ -67,32 +38,32 @@ $pagetitle = $strings[$lang]['title'];
     <div class="maingrid">
         <div class="main update-screen">
             <div style="text-align: right; margin-bottom: 1em;">
-                <?php if ($lang === 'pt'): ?>
-                    <a href="?lang=en">EN</a> | <span>PT</span>
+                <?php if ($lang === 'pt-PT'): ?>
+                    <a href="?lang=en-us">EN</a> | <span>PT</span>
                 <?php else: ?>
-                    <span>EN</span> | <a href="?lang=pt">PT</a>
+                    <span>EN</span> | <a href="?lang=pt-pt">PT</a>
                 <?php endif; ?>
             </div>
             <?php if ($update_result === null): ?>
                 <?php if ($needs_update): ?>
-                    <p><?= $strings[$lang]['db_needs_update'] ?></p>
-                    <p><?= $strings[$lang]['cannot_use_app'] ?></p>
-                    <p><?= $strings[$lang]['start_update'] ?></p>
+                    <p><?= $l10n['db_needs_update'] ?></p>
+                    <p><?= $l10n['cannot_use_app'] ?></p>
+                    <p><?= $l10n['start_update'] ?></p>
                     <p><?= $message ?></p>
                     <form method="POST" action="update.php?lang=<?= $lang ?>">
-                        <input class="submit" type="submit" name="action" value="<?= $strings[$lang]['form_button'] ?>">
+                        <input class="submit" type="submit" name="action" value="<?= $l10n['form_button'] ?>">
                     </form>
                 <?php else: ?>
-                    <p><?= $strings[$lang]['db_ok'] ?></p>
-                    <p><?= $strings[$lang]['go_login'] ?></p>
+                    <p><?= $l10n['db_ok'] ?></p>
+                    <p><?= $l10n['go_login'] ?></p>
                 <?php endif; ?>
             <?php elseif ($update_result): ?>
                 <p><?= $message ?></p>
-                <p><?= $strings[$lang]['db_updated'] ?></p>
-                <p><?= $strings[$lang]['redirecting'] ?></p>
+                <p><?= $l10n['db_updated'] ?></p>
+                <p><?= $l10n['redirecting'] ?></p>
             <?php else: ?>
-                <p><?= $strings[$lang]['update_fail'] ?></p>
-                <p><?= $strings[$lang]['error_msg'] ?><br><?= $message ?></p>
+                <p><?= $l10n['update_fail'] ?></p>
+                <p><?= $l10n['error_msg'] ?><br><?= $message ?></p>
             <?php endif; ?>
         </div>
     </div>
