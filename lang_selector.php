384e7567f2689cfa1643caff17181462218af961
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-08-15 16:09:12 +0100

    Add lang_selector.php file

---
+++ lang_selector.php
@@ -0,0 +1,7 @@
+<div >
+    <?php if (l10n::$lang === 'pt-pt'): ?>
+        <a href="?lang=en-us">EN</a> | <span>PT</span>
+    <?php else: ?>
+        <span>EN</span> | <a href="?lang=pt-pt">PT</a>
+    <?php endif; ?>
+</div>
