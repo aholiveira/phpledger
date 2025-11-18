a1fd2b8c6180d25a2e490f811df990584d70dab6
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-11-18 15:30:04 +0000

    Fix issues with the html menu accessibility

--- src/Util/Html.php
+++ src/Util/Html.php
@@ -118,25 +118,29 @@ class Html
         <aside class="menu">
             <nav>
                 <ul>
-                    <li><a id="ledger_entries"
-                            href="ledger_entries.php?lang=<?= l10n::$lang ?>"><?= l10n::l("ledger_entries") ?></a>
+                    <li><a id="ledger_entries" href="ledger_entries.php?lang=<?= l10n::$lang ?>"
+                            aria-label="<?= l10n::l("ledger_entries") ?>"><?= l10n::l("ledger_entries") ?></a>
                     </li>
-                    <li><a id="balance" href="balances.php?lang=<?= l10n::$lang ?>"><?= l10n::l("balances") ?></a>
+                    <li><a id="balance" href="balances.php?lang=<?= l10n::$lang ?>"
+                            aria-label="<?= l10n::l("balances") ?>"><?= l10n::l("balances") ?></a>
                     </li>
-                    <li><a id="accounts" href="accounts.php?lang=<?= l10n::$lang ?>"><?= l10n::l("accounts") ?></a>
+                    <li><a id="accounts" href="accounts.php?lang=<?= l10n::$lang ?>"
+                            aria-label="<?= l10n::l("accounts") ?>"><?= l10n::l("accounts") ?></a>
                     </li>
-                    <li><a id="account_type"
-                            href="account_types_list.php?lang=<?= l10n::$lang ?>"><?= l10n::l("account_types") ?></a>
+                    <li><a id="account_type" href="account_types_list.php?lang=<?= l10n::$lang ?>"
+                            aria-label="<?= l10n::l("account_types") ?>"><?= l10n::l("account_types") ?></a>
                     </li>
-                    <li><a id="entry_type" href="entry_types_list.php?lang=<?= l10n::$lang ?>"><?= l10n::l("entry_types") ?></a>
+                    <li><a id="entry_type" href="entry_types_list.php?lang=<?= l10n::$lang ?>"
+                            aria-label="<?= l10n::l("entry_types") ?>"><?= l10n::l("entry_types") ?></a>
                     </li>
-                    <li><a id="report_month"
-                            href="report_month.php?lang=<?= l10n::$lang ?>&year=<?= date("Y") ?>"><?= l10n::l("report_month") ?></a>
+                    <li><a id="report_month" href="report_month.php?lang=<?= l10n::$lang ?>&year=<?= date("Y") ?>"
+                            aria-label="<?= l10n::l("report_month") ?>"><?= l10n::l("report_month") ?></a>
                     </li>
-                    <li><a id="report_year"
-                            href="report_year.php?lang=<?= l10n::$lang ?>&year=<?= date("Y") - 1 ?>"><?= l10n::l("report_year") ?></a>
+                    <li><a id="report_year" href="report_year.php?lang=<?= l10n::$lang ?>&year=<?= date("Y") - 1 ?>"
+                            aria-label="<?= l10n::l("report_year") ?>"><?= l10n::l("report_year") ?></a>
                     </li>
-                    <li><a id="logout" href="index.php?lang=<?= l10n::$lang ?>&do_logout=1"><?= l10n::l("logout") ?></a>
+                    <li><a id="logout" href="index.php?lang=<?= l10n::$lang ?>&do_logout=1"
+                            aria-label="<?= l10n::l("logout") ?>"><?= l10n::l("logout") ?></a>
                 </ul>
             </nav>
         </aside>
