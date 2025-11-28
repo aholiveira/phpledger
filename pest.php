e81237ffcddbe19ab1383033264bd0f019ed6eb0
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-11-28 00:25:50 +0000

    fix(test): change require to require_once in pest.php

--- pest.php
+++ pest.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/tests/bootstrap.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/tests/bootstrap.php';
 
 uses(PHPLedgerTests\TestCase::class)->in('tests');
