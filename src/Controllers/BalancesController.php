fc202bfc16ad3b0fdc1402c2151251ae74255d4b
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-12-10 00:42:16 +0000

    Move Balances view to new ViewTemplate

--- src/Controllers/BalancesController.php
+++ src/Controllers/BalancesController.php
@@ -10,18 +10,81 @@
 
 namespace PHPLedger\Controllers;
 
+use PHPLedger\Domain\Account;
 use PHPLedger\Storage\ObjectFactory;
-use PHPLedger\Views\BalancesView;
-use PHPLedger\Views\ViewFactory;
+use PHPLedger\Util\Html;
+use PHPLedger\Util\NumberUtil;
+use PHPLedger\Views\Templates\BalancesViewTemplate;
 
 final class BalancesController extends AbstractViewController
 {
     protected function handle(): void
     {
         $object = ObjectFactory::account();
-        $viewer = ViewFactory::instance()->accountBalanceView($this->app, $object);
-        $reportData = $viewer->printObjectList($object->getList(['activa' => ['operator' => '=', 'value' => '1']]));
-        $view = new BalancesView;
-        $view->render($this->app, $reportData, $this->request->input('action'));
+        $objectList = $object->getList(['activa' => ['operator' => '=', 'value' => '1']]);
+        $type_names = ['income', 'expense', 'balance'];
+        $totals = array_fill_keys($type_names, 0);
+        $balances = [];
+
+        foreach ($objectList as $object) {
+            if ($object instanceof Account) {
+                $balances[$object->id] = $object->getBalanceOnDate(new \DateTime());
+                foreach ($type_names as $type_name) {
+                    $totals[$type_name] += $balances[$object->id][$type_name];
+                }
+            }
+        }
+        $l10n = [
+            'account' => $this->app->l10n()->l('account'),
+            'deposits' => $this->app->l10n()->l('deposits'),
+            'withdrawals' => $this->app->l10n()->l('withdrawals'),
+            'balance' => $this->app->l10n()->l('balance'),
+            'percent' => $this->app->l10n()->l('percent'),
+            'entries' => $this->app->l10n()->l('entries'),
+            'edit_account' => $this->app->l10n()->l('edit_account'),
+            'account_entries' => $this->app->l10n()->l('account_entries'),
+            'list' => $this->app->l10n()->l('list')
+        ];
+        $rows = [];
+        foreach ($objectList as $object) {
+            if ($object instanceof Account) {
+                $rows[] = [
+                    'text' => [
+                        'name' => $object->name,
+                        'deposits' => NumberUtil::normalize($balances[$object->id]['income']),
+                        'withdrawals' => NumberUtil::normalize($balances[$object->id]['expense']),
+                        'balance' => NumberUtil::normalize($balances[$object->id]['balance']),
+                        'percent' => NumberUtil::normalize($totals['balance'] <> 0 ? round($balances[$object->id]['balance'] / $totals['balance'] * 100, 2) : 0),
+                    ],
+                    'href' => [
+                        'name' => "index.php?action=account&back=balances&id={$object->id}",
+                        'entries' => "index.php?action=ledger_entries&filter_accountId={$object->id}"
+                    ]
+                ];
+            }
+        }
+        $rows[] = [
+            'text' => [
+                'name' => $this->app->l10n()->l('networth'),
+                'deposits' => NumberUtil::normalize($totals['income']),
+                'withdrawals' => NumberUtil::normalize($totals['expense']),
+                'balance' => NumberUtil::normalize($totals['balance']),
+                'percent' =>  NumberUtil::normalize(100),
+            ],
+            'href' => ['name' => '', 'entries' => '']
+        ];
+
+        $templateData = [
+            'title' => "Saldos",
+            'lang' => $this->app->l10n()->html(),
+            'app' => $this->app,
+            'action' => $this->request->input('action', 'balances'),
+            'isAdmin' => $this->app->session()->get('isAdmin', false),
+            'rows' => $rows,
+            'l10n' => $l10n,
+            'totals' => $totals,
+        ];
+        $view = new BalancesViewTemplate;
+        $view->render($templateData);
     }
 }
---
+++ src/Views/Templates/BalancesViewTemplate.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ *
+ * @author Antonio Henrique Oliveira
+ * @copyright (c) 2017-2022, Antonio Henrique Oliveira
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License (GPL) v3
+ *
+ */
+
+namespace PHPLedger\Views\Templates;
+
+use PHPLedger\Util\Html;
+
+final class BalancesViewTemplate extends AbstractViewTemplate
+{
+    public function render(array $data): void
+    {
+        extract($data, EXTR_SKIP);
+?>
+        <!DOCTYPE html>
+        <html lang="<?= $lang ?>">
+
+        <head>
+            <title><?= Html::title($title) ?></title>
+            <?php Html::header() ?>
+        </head>
+
+        <body>
+            <div class="maingrid">
+                <div id="preloader">
+                    <div class="spinner"></div>
+                </div>
+                <?php Html::menu($app->l10n(), $isAdmin); ?>
+                <div class="header" style="height: 0;"></div>
+                <div class="main" id="main">
+                    <div class="saldos">
+                        <table class="lista saldos">
+                            <thead>
+                                <tr>
+                                    <th><?= $l10n['account'] ?></th>
+                                    <th><?= $l10n['deposits'] ?></th>
+                                    <th><?= $l10n['withdrawals'] ?></th>
+                                    <th><?= $l10n['balance'] ?></th>
+                                    <th><?= $l10n['percent'] ?></th>
+                                    <th><?= $l10n['entries'] ?></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                foreach ($rows as $row):
+                                ?>
+                                    <tr>
+                                        <td class="account" data-label="<?= $l10n['account'] ?>">
+                                            <?= !empty($row['href']['name']) ? "<a title=\"{$l10n['edit_account']}\" href=\"{$row['href']['name']}\">" : "" ?>
+                                            <?= $row['text']['name'] ?>
+                                            <?= !empty($row['href']['name']) ? "</a>" : "" ?>
+                                        </td>
+                                        <td class="deposits" data-label="<?= $l10n['deposits'] ?>"><?= $row['text']['deposits'] ?></td>
+                                        <td class="withdrawals" data-label="<?= $l10n['withdrawals'] ?>"><?= $row['text']['withdrawals'] ?></td>
+                                        <td class="balance" data-label="<?= $l10n['balance'] ?>"><?= $row['text']['balance'] ?></td>
+                                        <td class="percent" data-label="<?= $l10n['percent'] ?>"><?= $row['text']['percent'] ?></td>
+
+                                        <td class="entries-list" data-label="<?= $l10n['entries'] ?>">
+                                            <?php if (!empty($row['href']['entries'])): ?>
+                                                <a title="<?= $l10n['account_entries'] ?>" href="<?= $row['href']['entries'] ?>"><?= $l10n['list'] ?></a>
+                                            <?php endif; ?>
+                                        </td>
+                                    </tr>
+                                <?php
+                                endforeach;
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <?php Html::footer($app, $action); ?>
+            </div>
+            <script>
+                setTimeout(() => {
+                    document.getElementById("preloader").style.display = "none";
+                }, 0);
+            </script>
+        </body>
+
+        </html>
+<?php
+    }
+}
