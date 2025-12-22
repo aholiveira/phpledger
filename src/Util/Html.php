8b183096e4088c229b76f2571a43746afbdd3ee8
aholiveira <98539014+aholiveira@users.noreply.github.com>
2025-12-22 22:13:42 +0000

    Remove extraneous tilde in HTML class

--- src/Util/Html.php
+++ src/Util/Html.php
@@ -52,7 +52,7 @@ final class Html
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script defer src="assets/set-timezone.js"></script>
         <link rel="stylesheet" href="assets/styles.css">
-        <link rel="icon" type="image/x-icon" href="assets/logo-2.ico">~
+        <link rel="icon" type="image/x-icon" href="assets/logo-2.ico">
         <link rel="shortcut icon" type="image/x-icon" href="assets/logo-2.ico">
 <?php
     }
